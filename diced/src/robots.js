5921be1ee1e8e568f0956ce85e1529aca1ae60b8
Kamen Boev <kamen.boev@gmail.com>
2019-02-14 14:48:15 +0000

    setting up React app with Card components

---
+++ diced/src/robots.js
@@ -0,0 +1,75 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Jeremmy Kragle',
+        username: 'Jerry',
+        email: 'goforitnow@majuna.org'
+    },
+    {
+        id: 2,
+        name: 'Jon Snow',
+        username: 'bastardo',
+        email: 'wallguy@majuna.org'
+    },
+    {
+        id: 3,
+        name: 'Maurice Watson',
+        username: 'Watsy',
+        email: 'mwwjs@kahuna.net'
+    },
+     {
+        id: 4,
+        name: 'Conor McGregor',
+        username: 'Notorious',
+        email: 'peopleschamp@ufcna.org'
+    },
+    {
+        id: 5,
+        name: 'Kyle Gassy',
+        username: 'sniper',
+        email: 'gobantaweightw@yahoo.org'
+    },
+    {
+        id: 6,
+        name: 'Jet Fay',
+        username: 'Jay',
+        email: 'gotrocksw@madjuna.org'
+    },
+    {
+        id: 7,
+        name: 'Kag Black',
+        username: 'Jerry',
+        email: 'salsa@majuna.org'
+    },
+    {
+        id: 8,
+        name: 'Krill Marries',
+        username: 'Merry',
+        email: 'grumba@majuna.org'
+    },
+    {
+        id: 9,
+        name: 'Bend Slope',
+        username: 'Larry',
+        email: 'goforitnow@majuna.org'
+    },
+     {
+        id: 10,
+        name: 'Kisdf Dzjerdzjinski',
+        username: 'Wikiy',
+        email: 'gasdf@majuna.org'
+    },
+    {
+        id: 11,
+        name: 'Bechy Kragle',
+        username: 'Bechy',
+        email: 'becritnow@majuna.org'
+    },
+    {
+        id: 12,
+        name: 'Jeremmy Kragle',
+        username: 'Jerry',
+        email: 'goforitnow@majuna.org'
+    }
+    
+]
